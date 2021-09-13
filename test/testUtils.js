eca1448566dc453c9e86be1a9aad5e3206b19bdd
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-09-13 12:40:10 +0300

    testUtils.js

---
+++ test/testUtils.js
@@ -0,0 +1,21 @@
+import checkPropTypes from 'check-prop-types';
+
+/**
+ * Return node(s) with the given data-test attribute.
+ * @param {ShallowWrapper} wrapper - enzyme shallow wrapper.
+ * @param {string} val = Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+export const findByTestAttr = (wrapper, val) => {
+    return wrapper.find(`[data-test='${val}']`)
+};
+
+export const checkProps = (component, confimingProps) => {
+    const propError = checkPropTypes(
+        component.propTypes,
+        confimingProps,
+        'prop',
+        component.name
+    )
+    expect(propError).toBeUndefined();
+}
