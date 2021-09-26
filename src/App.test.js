43480fc5a572301af927ed82b8aa053b0aea745f
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-09-26 13:21:19 +0300

    restart-every-thing

--- src/App.test.js
+++ src/App.test.js
@@ -1,19 +1,8 @@
-import { shallow } from 'enzyme';
-import { findByTestAttr } from '../test/testUtils';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-
-/**
- * Setup function for App component
- * @returns {ShallowWrapper}
- */
-const setup = () => {
-  return shallow(<App />);
-}
-
-
-test('renders without error', () => {
-  const wrapper = setup();
-  const app = findByTestAttr(wrapper, 'app-component');
-  expect(app).toHaveLength(1);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
