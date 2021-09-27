ae9f8e17852cf8e43d45ef37242aa622c2b87254
Mor Mordechai Ben <31767946+morMBen@users.noreply.github.com>
2021-09-27 15:40:58 +0300

    specializing component

--- src/components/Specializing/index.js
+++ src/components/Specializing/index.js
@@ -2,18 +2,72 @@ import Card from '../UI/Card';
 import MediumHeading from '../UI/MediumHeading';
 import ProgressBar from '../UI/ProgressBar/index';
 import SmallHeading from '../UI/SmallHeading';
+import { colors } from '../../style';
 
 export const Specializing = (props) => {
+
+    const skills = [{
+        skillName: 'HTML/CSS',
+        description: 'Hands on experience in Html/Css',
+        value: 80,
+        textColor: colors.textColor,
+        pathColor: '#E96228'
+    }, {
+        skillName: 'Java Script',
+        description: 'Hands on experience in Java Script',
+        value: 90,
+        textColor: colors.textColor,
+        pathColor: '#EFD81D'
+    }, {
+        skillName: 'ReactJS/Redux',
+        description: 'Hands on experience in React/Redux',
+        value: 85,
+        textColor: colors.textColor,
+        pathColor: '#5ED3F3'
+    }, {
+        skillName: 'NodeJS/Express',
+        description: 'Hands on experience in NodeJS/Express',
+        value: 70,
+        textColor: colors.textColor,
+        pathColor: '#43853D'
+    }, {
+        skillName: 'MongoDB/Mongoose',
+        description: 'Hands on experience in MongoDB/Mongoose',
+        value: 65,
+        textColor: colors.textColor,
+        pathColor: '#13AA52'
+    }, {
+        skillName: 'Flutter/Dart',
+        description: 'Hands on experience in Flutter/Dart',
+        value: 55,
+        textColor: colors.textColor,
+        pathColor: '#0276E8'
+    }, {
+        skillName: 'Jest/Enzyme',
+        description: 'Hands on experience in Jest/Enzyme',
+        value: 50,
+        textColor: colors.textColor,
+        pathColor: '#C03B13'
+    },]
+
     return (
         <div className='container'>
-            <Card style={{ padding: '40px' }}>
+            <Card style={{ padding: '0px' }}>
                 <SmallHeading text='what i do' />
                 <MediumHeading text='specializing in' />
-                <ProgressBar
-                    textColor='red'
-                    pathColor='blue'
-                    trailColor='black'
-                    value='40' />
+                <div className='flex-row wrap justify-sb' style={{ padding: '30px' }}>
+                    {skills.map((skill, i) => {
+                        return <ProgressBar
+                            key={i}
+                            skillName={skill.skillName}
+                            description={skill.description}
+                            textColor={skill.textColor}
+                            pathColor={skill.pathColor}
+                            trailColor='#D0D2D4'
+                            value={skill.value} />
+                    })}
+
+                </div>
             </Card>
         </div>
     )
--- src/components/UI/ProgressBar/index.js
+++ src/components/UI/ProgressBar/index.js
@@ -1,31 +1,42 @@
 import { CircularProgressbarWithChildren, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import RadialSeparators from './RadialSeparators';
+import './style.css'
 
-const ProgressBar = ({ textColor, pathColor, trailColor, value }) => {
+const ProgressBar = ({ textColor, pathColor, trailColor, value, skillName, description }) => {
+
+
     return (
-        <CircularProgressbarWithChildren
-            value={value}
-            text={`${value}%`}
-            strokeWidth={10}
-            styles={buildStyles({
-                textColor: textColor,
-                pathColor: pathColor,
-                strokeLinecap: "butt",
-                trailColor: trailColor
+        <div className='flex-row align-center mtb-10' style={{ width: '50%' }}>
+            <div className='skill-container'>
+                <CircularProgressbarWithChildren
+                    value={value}
+                    text={`${value}%`}
+                    strokeWidth={10}
+                    styles={buildStyles({
+                        textColor: textColor,
+                        pathColor: pathColor,
+                        strokeLinecap: "butt",
+                        trailColor: trailColor
 
-            })}
-        >
-            <RadialSeparators
-                count={12}
-                style={{
-                    background: "#fff",
-                    width: "5px",
-                    // This needs to be equal to props.strokeWidth
-                    height: `${10}%`
-                }}
-            />
-        </CircularProgressbarWithChildren>
+                    })}
+                >
+                    <RadialSeparators
+                        count={12}
+                        style={{
+                            background: "#fff",
+                            width: "1px",
+                            // This needs to be equal to props.strokeWidth
+                            height: `${10}%`
+                        }}
+                    />
+                </CircularProgressbarWithChildren>
+            </div>
+            <div className='mlr-10'>
+                <p className='font-18 bold-500 text-color mtb-10'>{skillName}</p>
+                <p className='font-14 bold-500 grey mtb-10'>{description}</p>
+            </div>
+        </div>
     )
 }
 export default ProgressBar;
